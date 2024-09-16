803242f5918f443c6de00559c15b2f31b37ad63b
AldoAragon2710 <aldomaxito@hotmail.com>
2024-09-16 17:47:07 -0600

    Contraseñas aleatorias con Señalamientos

--- Proyecto-Base Creacion de Claves DEV3/main.js
+++ Proyecto-Base Creacion de Claves DEV3/main.js
@@ -1,31 +1,51 @@
-let cantidad = document.getElementById("cantidad"); 
-let boton = document.getElementById("generar");
-let texto = "texto";
-let contraseña = document.getElementById("contrasena");
-
-const cadenacaracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$/&?¡*";
-
+let cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+let contrasena = document.getElementById('contrasena');
+let messageElement = document.getElementById('message');
 function generar(){
     let numeroDigitado = parseInt(cantidad.value);
-
-    console.log(numeroDigitado);
-   
-    if(numeroDigitado < 8){
-        alert("La cantidad de caracteres debe ser mayor o igual a 8");
-    }  
-    
-    let password = "";
-    
-    for(let i = 0; i < numeroDigitado; i++){
-
-        let caracterAleatorio = cadenacaracteres [Math.floor (Math.random() * cadenacaracteres.length)];
-        console.log(caracterAleatorio);
-
-        password+= caracterAleatorio;
-
+    if(numeroDigitado<8){
+       return alert("La cantidad debe ser mayor a 8");
     }
-    contraseña.value = password;
-} 
+    let password = '';
+    for(let i = 0; i<numeroDigitado; i++){
+        let caracterAleatorio = caracteres[Math.floor(Math.random() * caracteres.length)];
+        password+= caracterAleatorio;
+    }
+    contrasena.value = password;
+    validarContrasena(password);
+    
+
+}
+function limpiar(){
+    cantidad.value = '';
+    contrasena.value = '';
+    messageElement.textContent = '';
+    messageElement.classList.remove('weak');
+    messageElement.classList.remove('strong');
+}
+function validarContrasena(contrasenaGenerada){
+    console.log(contrasenaGenerada);
+    let numeros = /\d/;
+    let letraMayuscula = /[A-Z]/;
+    let validacion= false;
+
+    if (numeros.test(contrasenaGenerada) && letraMayuscula.test(contrasenaGenerada)) {
+        validacion = true;
+    }
+
+    if (validacion) {
+        messageElement.textContent = 'Contraseña fuerte';
+        messageElement.classList.remove('weak');
+        messageElement.classList.add('strong');
+      } else {
+        messageElement.textContent = 'Contraseña débil. Debe tener al menos una letra mayúscula y un número.';
+        messageElement.classList.remove('strong');
+        messageElement.classList.add('weak');
+      }
+}
+
 
 
 
