0aba38f2be99e0e9c90f9fb67e4a2caf0b367f50
AldoAragon2710 <aldomaxito@hotmail.com>
2024-09-16 17:51:59 -0600

    Creador de Contraseñas Online

--- Proyecto-Base Creacion de Claves DEV3/main.js
+++ Proyecto-Base Creacion de Claves DEV3/main.js
@@ -1,12 +1,12 @@
 let cantidad = document.getElementById('cantidad');
 let boton = document.getElementById('generar');
-let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+let caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?¡';
 let contrasena = document.getElementById('contrasena');
 let messageElement = document.getElementById('message');
 function generar(){
     let numeroDigitado = parseInt(cantidad.value);
     if(numeroDigitado<8){
-       return alert("La cantidad debe ser mayor a 8");
+       return alert("los caracteres deben ser mayor o igual a 8");
     }
     let password = '';
     for(let i = 0; i<numeroDigitado; i++){
@@ -36,11 +36,11 @@ function validarContrasena(contrasenaGenerada){
     }
 
     if (validacion) {
-        messageElement.textContent = 'Contraseña fuerte';
+        messageElement.textContent = 'Contraseña Segura';
         messageElement.classList.remove('weak');
         messageElement.classList.add('strong');
       } else {
-        messageElement.textContent = 'Contraseña débil. Debe tener al menos una letra mayúscula y un número.';
+        messageElement.textContent = 'Contraseña débil. Debe tener al menos una letra mayúscula y un caracter especial.';
         messageElement.classList.remove('strong');
         messageElement.classList.add('weak');
       }
